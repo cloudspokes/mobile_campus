e58f8df4088d9aee265df743d6f3d6ab79aacdea
Mauricio Desiderio <mdesiderio@appirio.com>
2012-07-31 16:49:15 -0500

    adding webviewer plugin to android app

---
+++ aau/android/AAU Campus/src/com/appirio/mobile/WebViewActivity.java
@@ -0,0 +1,36 @@
+package com.appirio.mobile;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.Window;
+import android.webkit.WebView;
+
+import com.appirio.aau.R;
+import com.appirio.mobile.plugins.AppirioWebViewClient;
+
+public class WebViewActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_web_view);
+        
+        WebView v = (WebView)findViewById(R.id.webView); 
+        
+        v.setWebViewClient(new AppirioWebViewClient());
+        
+        v.getSettings().setSupportZoom(true);
+        
+        v.loadUrl(getIntent().getStringExtra("url"));
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_web_view, menu);
+        return false;
+    }
+
+    
+}
---
+++ aau/android/AAU Campus/src/com/appirio/mobile/plugins/AMWebViewer.java
@@ -0,0 +1,29 @@
+package com.appirio.mobile.plugins;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+
+import android.content.Intent;
+
+import com.appirio.mobile.WebViewActivity;
+import com.phonegap.api.Plugin;
+import com.phonegap.api.PluginResult;
+
+public class AMWebViewer extends Plugin {
+
+	@Override
+	public PluginResult execute(String action, JSONArray params, String arg2) {
+
+		try {
+			Intent i = new Intent(this.ctx, WebViewActivity.class);
+			i.putExtra("url", params.getString(0));
+			
+			ctx.startActivity(i);
+		} catch (JSONException e) {
+			e.printStackTrace();
+		}
+
+		return new PluginResult(PluginResult.Status.NO_RESULT);
+	}
+
+}

04ada174bae8dbe7392f6962c5f65406adb4a516
Mauricio Desiderio <mdesiderio@appirio.com>
2013-01-09 15:01:13 -0600

    cleaning assets folder

--- aau/android/AAUCampus_SFSDK1.3/bin/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/Users/mauriciodesiderio/work/aau_git/aau/android/AAUCampus_SFSDK1.3/bin/AndroidManifest.xml : \
-/Users/mauriciodesiderio/work/aau_git/aau/android/AAUCampus_SFSDK1.3/AndroidManifest.xml \
--- aau/android/SalesforceSDK/bin/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/Users/mauriciodesiderio/work/aau_git/aau/android/SalesforceSDK/bin/AndroidManifest.xml : \
-/Users/mauriciodesiderio/work/aau_git/aau/android/SalesforceSDK/AndroidManifest.xml \

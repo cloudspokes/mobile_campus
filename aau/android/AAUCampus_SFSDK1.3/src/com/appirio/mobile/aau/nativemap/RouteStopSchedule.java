3bce9ed2b54be6f28dfd8420f4af2c149bbe580b
Mauricio Desiderio <mdesiderio@appirio.com>
2013-02-14 14:03:21 -0600

    working on the stop detail popup

--- aau/android/AAUCampus_SFSDK1.3/src/com/appirio/mobile/aau/nativemap/RouteStopSchedule.java
+++ aau/android/AAUCampus_SFSDK1.3/src/com/appirio/mobile/aau/nativemap/RouteStopSchedule.java
@@ -1,16 +1,116 @@
 package com.appirio.mobile.aau.nativemap;
 
 import java.io.Serializable;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
 
+import org.json.JSONException;
 import org.json.JSONObject;
+
+import android.text.format.DateFormat;
 
 public class RouteStopSchedule implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 
-	public RouteStopSchedule(JSONObject object) {
+	private String color;
+	private String name;
+	private List<String> schedule;
+	private SimpleDateFormat format = new SimpleDateFormat("h:mm a");
+	
+	public RouteStopSchedule(JSONObject object) throws AMException {
+		try {
+			if(object.has("color")) {
+				this.color = object.getString("color");
+			}
+			
+			if(object.has("name")) {
+				this.name = object.getString("name");
+			}
+			
+			schedule = new ArrayList<String>();
+			
+			if(object.has("schedule")) {
+				for(int i = 0; i < object.getJSONArray("schedule").length(); i++) {
+					schedule.add(object.getJSONArray("schedule").getString(i));
+				}
+			}
+		} catch (JSONException e) {
+			e.printStackTrace();
+			
+			throw new AMException(e);
+		}
+	}
 
+	public String getColor() {
+		return color;
+	}
+
+	public void setColor(String color) {
+		this.color = color;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public List<String> getSchedule() {
+		return schedule;
+	}
+
+	public void setSchedule(List<String> schedule) {
+		this.schedule = schedule;
 	}
 	
+	public String getNextBusETA() {
+		return "3 minutes";
+	}
+	
+	public String getTodaysNextStops() {
+		StringBuilder result = new StringBuilder();
+		String separator = "";
+		
+		int nextStopIndex = getNextStopIndex();
+		
+		for(String time : schedule) {
+			result.append(separator);
+			result.append(time);
+			
+			separator = ", ";
+		}
+		
+		return result.toString();
+	}
+	
+	private int getNextStopIndex() {
+		try {
+			Calendar now = Calendar.getInstance();
+			
+			now.set(1970, 0, 1);
+			
+			for(int i = 0; i < schedule.size(); i++) {
+				Date date = format.parse(schedule.get(i));
+				
+				
+				if(date.after(now.getTime())) {
+					return i;
+				}
+			}
+			
+			return -1;
+		} catch (ParseException e) {
+			e.printStackTrace();
+			
+			return -1;
+		}
+	}
 	
 }
--- aau/android/AAUCampus_SFSDK1.3/src/com/appirio/mobile/aau/nativemap/StopScheduleActivity.java
+++ aau/android/AAUCampus_SFSDK1.3/src/com/appirio/mobile/aau/nativemap/StopScheduleActivity.java
@@ -3,8 +3,10 @@ package com.appirio.mobile.aau.nativemap;
 import java.util.ArrayList;
 
 import android.app.Activity;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
 import android.view.ViewGroup;
 import android.view.Window;
 import android.widget.TextView;
@@ -21,14 +23,23 @@ public class StopScheduleActivity extends Activity {
 		setContentView(R.layout.activity_stop_schedule);
 		
 		ViewGroup root = (ViewGroup) this.findViewById(R.id.mainStopScheduleContainer);
+		ViewGroup stopSchedulecontainer = (ViewGroup) this.findViewById(R.id.stopScheduleContainer);
 
 		((TextView)this.findViewById(R.id.txtBusStopName)).setText(getIntent().getExtras().getString("stopName"));
 		
 		ArrayList<RouteStopSchedule> schedule = (ArrayList<RouteStopSchedule>) getIntent().getExtras().get("schedule");
 		
 		for(RouteStopSchedule routeSchedule : schedule) {
-			root.addView(this.getLayoutInflater().inflate(R.layout.schedule_item, null));
-		
+			View scheduleView = this.getLayoutInflater().inflate(R.layout.schedule_item, null);
+			
+			((TextView)scheduleView.findViewById(R.id.txtRouteName)).setText(routeSchedule.getName());
+			((TextView)scheduleView.findViewById(R.id.txtRouteName)).setBackgroundColor(Color.parseColor(routeSchedule.getColor()));
+			
+			((TextView)scheduleView.findViewById(R.id.txtETA)).setText(routeSchedule.getNextBusETA());
+			
+			((TextView)scheduleView.findViewById(R.id.txtUpcomingStops)).setText(routeSchedule.getTodaysNextStops());
+
+			stopSchedulecontainer.addView(scheduleView);
 		}
 	}
 
